0b0798cd8b3fa2a0e4e51c3ef24dd76af6b9efaa
DavitBoo <davidmarras@gmail.com>
2023-07-25 17:44:09 +0200

    using express

--- index.js
+++ index.js
@@ -1,41 +1,69 @@
-const http = require("http");
-const fs = require("fs");
-const url = require("url");
-// const path = require("path");
+const path = require("path");
 
-http
-  .createServer((req, res) => {
-    if (req.url === "/favicon.ico") {
-      res.writeHead(200, { "Content-Type": "image/x-icon" });
-      return res.end();
-    }
+const express = require("express");
+const app = express();
 
-    const parsedUrl = url.parse(req.url, true);
-    const pathname = parsedUrl.pathname;
+app.get("/", (req, res) => {
+  // res.writeHead(200, { "Content-Type": "image/x-icon" });
+  res.sendFile(path.join(__dirname, "/index.html"));
+  //       return res.end();
+});
 
-    let contentType = "text/html";
-    let filePath = "";
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "/about.html"));
+});
 
-    if (pathname === "/") {
-      filePath = "./index.html";
-    } else if (pathname === "/about" || pathname === "/contact-me") {
-      filePath = `.${pathname}.html`;
-    } else if (pathname === "/style.css") {
-      filePath = "./style.css";
-      contentType = "text/css";
-    } else {
-      filePath = "./404.html";
-      res.writeHead(404, { "Content-Type": "text/html" });
-    }
+app.get("/contact-me", (req, res) => {
+  res.sendFile(path.join(__dirname, "/contact-me.html"));
+});
 
-    fs.readFile(filePath, (err, content) => {
-      if (err) {
-        res.writeHead(500, { "Content-Type": "text/html" });
-        res.end("Error interno del servidor");
-      } else {
-        res.writeHead(200, { "Content-Type": contentType });
-        res.end(content);
-      }
-    });
-  })
-  .listen(8080);
+app.get("/style.css", (req, res) => {
+  res.sendFile(path.join(__dirname, "style.css"));
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "404.html"));
+});
+
+const port = 8080;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
+
+// http
+//   .createServer((req, res) => {
+//     if (req.url === "/favicon.ico") {
+//       res.writeHead(200, { "Content-Type": "image/x-icon" });
+//       return res.end();
+//     }
+
+//     const parsedUrl = url.parse(req.url, true);
+//     const pathname = parsedUrl.pathname;
+
+//     let contentType = "text/html";
+//     let filePath = "";
+
+//     if (pathname === "/") {
+//       filePath = "./index.html";
+//     } else if (pathname === "/about" || pathname === "/contact-me") {
+//       filePath = `.${pathname}.html`;
+//     } else if (pathname === "/style.css") {
+//       filePath = "./style.css";
+//       contentType = "text/css";
+//     } else {
+//       filePath = "./404.html";
+//       res.writeHead(404, { "Content-Type": "text/html" });
+//     }
+
+//     fs.readFile(filePath, (err, content) => {
+//       if (err) {
+//         res.writeHead(500, { "Content-Type": "text/html" });
+//         res.end("Error interno del servidor");
+//       } else {
+//         res.writeHead(200, { "Content-Type": contentType });
+//         res.end(content);
+//       }
+//     });
+//   })
+//   .listen(8080);
